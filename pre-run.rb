72c098904748611e437804a338574e08dacd38fd
Clint <clinthiles1992@gmail.com>
2025-08-14 07:51:25 -0700

    Adds shoe and book data structures for organization
    Introduces new data structures for shoes and books to enhance organization within the application.
    
    The changes include the creation of a hash for shoes categorized by season and a hash for books with their respective authors.
    
    Additionally, some previously defined arrays and logic have been commented out for future reference or potential use.

--- pre-run.rb
+++ pre-run.rb
@@ -1,12 +1,42 @@
-num_arr1 = Array(1..10)
-num_arr2 = Array(5..10)
-str_arr = Array("a".."z")
 
-common = num_arr1.concat(num_arr2)
-p common.count(7)
-p str_arr
 
-str_arr.each_with_index do |e, i|
-  p "#{i}: #{e}"
-end
 
+# shoes = Hash.new
+
+# shoes["hiking"] = "Hiking boot"
+# shoes['summer'] = "Sandals"
+# shoes['fall'] = 'Runners'
+# shoes['winter'] = 'Boots'
+# shoes['Fall'] = 'Runners'
+
+# books = {
+#   "LOTR" => 'J.R Tolkein',
+#   "The Shining" => "Stephen King"
+# }
+
+# merged = Hash.new
+
+# merged['shoes'] = shoes
+# merged['books'] = books
+
+# p merged['shoes'].keys
+# Array Practice
+
+
+#num_arr1 = Array(1..10)
+# num_arr2 = Array(5..10)
+# str_arr = Array("a".."z")
+
+# common = num_arr1.concat(num_arr2)
+# p common.count(7)
+# p str_arr
+
+# new_list = []
+# num_arr1.filter do |n|
+#   new_list.push(n.even?)
+# end
+
+
+
+# p new_list
+# p "False appears #{new_list.count(false)} times"
