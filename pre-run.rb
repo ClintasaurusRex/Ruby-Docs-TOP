85507cba29ddc0243d1ee11e8903b6ec0bcd20d4
Clint <clinthiles1992@gmail.com>
2025-09-02 07:58:01 -0700

    Enhance documentation for nested arrays and hashes, including examples of using #dig method for safe access

--- pre-run.rb
+++ pre-run.rb
@@ -17,6 +17,13 @@ immutable = Array.new(3) { Array.new(2) }
 immutable[0][0] = 1000
 puts immutable.inspect
 puts "-------------------------------------------------------------------"
+# If you want a nil value returned when trying to access an index of a nonexistent nested element, you can use the #dig method. This method can also be used when accessing a nonexistent index inside of an existing nested element.
+teacher_mailboxes.dig(3, 0)
+#=> nil
+teacher_mailboxes.dig(0, 4)
+#=> nil
+
+
 # Just to show that this will add to the nested array
 immutable[0][1] = 100
 puts immutable.inspect
@@ -65,3 +72,30 @@ check = test_scores.any? do |scores|
   scores.all? { |score| score > 80 }
 end
 puts check
+puts "-------------------------------------------------------------------"
+# Nested Hashes
+vehicles = {
+  alice: {year: 2019, make: "Toyota", model: "Corolla"},
+  blake: {year: 2020, make: "Volkswagen", model: "Beetle"},
+  caleb: {year: 2020, make: "Honda", model: "Accord"}
+}
+
+# Accessing data
+# Accessing a specific element in a nested hash is very similar to a nested array. It is done by calling hash[:x][:y], where :x is the key of the hash and :y is the key of the nested hash.
+vehicles[:alice][:year]
+#=> 2019
+vehicles[:blake][:make]
+#=> "Volkswagen"
+vehicles[:caleb][:model]
+#=> "Accord"
+
+# Similar to nested arrays, if you try to access a key in a nonexistent nested hash, it will raise an NoMethodError, therefore you may want to use the #dig method. As expected, if you try to access a nonexistent key in an existing nested hash, it will return nil.
+
+vehicles[:zoe][:year]
+#=> NoMethodError
+vehicles.dig(:zoe, :year)
+#=> nil
+vehicles[:alice][:color]
+#=> nil
+vehicles.dig(:alice, :color)
+#=> nil
