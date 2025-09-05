532a3a5c3783a7cc8a64a8d81e17e2fc7d22169b
Clint <clinthiles1992@gmail.com>
2025-09-05 07:57:18 -0700

    Refactor substrings method for improved readability by adding inline comments

--- pub_strings.rb
+++ pub_strings.rb
@@ -2,14 +2,14 @@ dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i",
 
 def substrings(str, dictionary)
   # Create an MT hash
-  result = Hash.new(0)
+  result = Hash.new(0) # create the hash so that all elements 
   str_input = Array.new(str.downcase.split(" "))
 
   #Compare the input to the dictionary
-  str_input.each do |word|
-    dictionary.each do |dict_word|
-      if word.include?(dict_word)
-        result[dict_word] += 1
+  str_input.each do |word| # Loop through the input
+    dictionary.each do |dict_word| # Loop through the dictionary
+      if word.include?(dict_word) # Compare the input to the words in the dictionary
+        result[dict_word] += 1 # Increment the times the word shows up into the our dictionary
       end
     end
   end
@@ -17,3 +17,8 @@ def substrings(str, dictionary)
 end
 
 puts substrings("below", dictionary).inspect
+
+def stock_picker(stocks)
+  result = Arran.new()
+  
+end
